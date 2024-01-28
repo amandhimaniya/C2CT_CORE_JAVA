9ab87c49d1ff8825f47c602132f147dbd158b910
Aman Kumar <amanparjapati2708@gmail.com>
2024-01-28 22:03:55 +0530

    First Commit

---
+++ Salesforce/src/com/tnsif/practices/SwipeNumbers.java
@@ -0,0 +1,17 @@
+package com.tnsif.practices;
+
+public class SwipeNumbers {
+
+	public static void main(String[] args) {
+		int num1=5;
+		int num2=6;
+		int temp;
+		
+		temp=num1;
+		num1=num2;
+		num2=temp;
+		System.out.println("a="+num1 + " b="+num2);
+
+	}
+
+}
